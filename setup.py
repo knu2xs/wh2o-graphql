4e2a373b03af865ea570ea03e3699a4e7f459740
Joel McCune <knu2xs@gmail.com>
2020-08-05 14:46:29 -0700

    escape aphostrophe in setup so install dev works

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages('src'),
     version='0.1.0',
-    description='Python bindings for American Whitewater's GraphQL endpoint.',
+    description='Python bindings for American Whitewater\'s GraphQL endpoint.',
     long_description=long_description,
     author='Joel McCune',
     license='Apache 2.0',
